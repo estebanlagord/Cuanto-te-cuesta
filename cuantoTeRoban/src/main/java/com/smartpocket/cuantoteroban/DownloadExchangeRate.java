348bf6bea451581dd97be17814b14f345369dfc8
Esteban Lagord <estebanlagord@gmail.com>
2020-01-12 10:04:30 -0300

    Removed references to Yahoo Finances

--- cuantoTeRoban/src/main/java/com/smartpocket/cuantoteroban/DownloadExchangeRate.java
+++ cuantoTeRoban/src/main/java/com/smartpocket/cuantoteroban/DownloadExchangeRate.java
@@ -26,6 +26,7 @@ import java.util.Locale;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+@Deprecated
 class DownloadExchangeRate extends	AsyncTask<String, Integer, String> {
 	
 	public static boolean updateInProgress = false;
